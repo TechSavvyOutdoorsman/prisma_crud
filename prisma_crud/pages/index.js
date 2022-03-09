bc8bbd9a5e161aeb1192b35aa9a047308f39adcb
techsavvyoutdoorsman <bramdonovan@gmail.com>
2022-03-08 19:39:53 -0700

    init

---
+++ prisma_crud/pages/index.js
@@ -0,0 +1,18 @@
+import { 
+  Text, 
+  Heading, 
+  Flex,
+
+} from '@chakra-ui/react'
+
+
+
+export default function Home() {
+  return (
+    <Flex>
+      <Flex flexDir='column' textAlign='center' align='center'>
+        <Heading>This is our Heading</Heading>
+      </Flex>
+    </Flex>
+  )
+}
