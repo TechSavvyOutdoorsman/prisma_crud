b0b512b89a505f40b421c9d0cee752b6e566a48c
techsavvyoutdoorsman <bramdonovan@gmail.com>
2022-03-08 20:31:02 -0700

    okay

---
+++ prisma_crud/components/layouts/Main.js
@@ -0,0 +1,27 @@
+import Head from 'next/head'
+import { Children } from 'react'
+import {
+    Box,
+    Heading,
+    Text
+} from '@chakra-ui/react'
+
+
+const Layout = ({ children }) => {
+
+
+    return (
+        <>
+          <Head>
+              <title>Prisma Crud App</title>
+          </Head>  
+          <Box>
+            {children}
+          </Box>
+
+        </>
+    )
+}
+
+
+export default Layout
--- prisma_crud/pages/index.js
+++ prisma_crud/pages/index.js
@@ -4,15 +4,23 @@ import {
   Flex,
 
 } from '@chakra-ui/react'
+import Layout from '../components/layouts/Main'
 
 
 
 export default function Home() {
   return (
-    <Flex>
-      <Flex flexDir='column' textAlign='center' align='center'>
-        <Heading>This is our Heading</Heading>
+    <Layout>
+      <Flex align='center' justify='center' flexDir='column'>
+        <Flex mt={5} flexDir='column' align='center' align='center'>
+          <Heading as='h1' justify='center'>This is a Prisma Crud App with Sqlite</Heading>
+        </Flex>
+        <Flex>
+
+        </Flex>
       </Flex>
-    </Flex>
+    </Layout>
   )
 }
+
+
